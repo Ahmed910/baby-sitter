c10e789670845c7f7bb3c1431047cc4029687b5c
Ahmed910 <ahmedabotaleb256@gmail.com>
2022-01-30 17:16:05 +0200

    edit

--- app/Traits/Order.php
+++ app/Traits/Order.php
@@ -199,7 +199,7 @@ trait Order
     private function getAppProfit($totol_price_for_order)
     {
         $financial = [];
-        $financial['app_profit_percentage'] = (double)setting('app_profit_percentage');
+        $financial['app_profit_percentage'] = setting('app_profit_percentage') != null ? (double)setting('app_profit_percentage'):0 ;
         $financial['app_profit'] = $totol_price_for_order * ($financial['app_profit_percentage']/100);
         $financial['final_price'] = $totol_price_for_order - $financial['app_profit'];
         return $financial;
