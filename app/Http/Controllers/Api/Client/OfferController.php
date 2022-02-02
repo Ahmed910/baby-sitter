d142eef4ef0b391db7d8cc9a578c982c589f7602
Ahmed910 <ahmedabotaleb256@gmail.com>
2022-02-02 17:12:13 +0200

    commit

--- app/Http/Controllers/Api/Client/OfferController.php
+++ app/Http/Controllers/Api/Client/OfferController.php
@@ -20,11 +20,8 @@ class OfferController extends Controller
         $orders_with_that_offer = MainOrder::where('offer_id',$offer->id)->count();
         if($orders_with_that_offer <= $offer->max_num)
         {
-           $financials_data['discount'] = $offer->discount;
-           $financials_data['total_price_before_discount'] = (float)$request->order_price;
-           $financials_data['total_price_after_discount'] = $financials_data['total_price_before_discount'] - $financials_data['discount'];
-           return response()->json(['financial_data'=>$financials_data,'status'=>'success','message'=>'']);
-
+           return (new OfferResource($offer))->additional(['status'=>'success','message'=>'']);
+        //    return response()->json(['financial_data'=>$financials_data,'status'=>'success','message'=>'']);
         }
         return response()->json(['data'=>null,'status'=>'fail','message'=>trans('api.messages.the_max_num_for_offer_has_been_used')],401);
     }
