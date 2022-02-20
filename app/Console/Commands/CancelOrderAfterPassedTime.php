541c3ea43872de607d8aeed4c72ad14a53569742
Ahmed910 <ahmedabotaleb256@gmail.com>
2022-02-20 14:30:53 +0200

    edit cron jobs

--- app/Console/Commands/CancelOrderAfterPassedTime.php
+++ app/Console/Commands/CancelOrderAfterPassedTime.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Classes\Statuses;
 use App\Models\CenterOrder;
+use App\Models\OrderMonthDate;
 use App\Models\SitterOrder;
 use App\Traits\Order;
 use Illuminate\Console\Command;
@@ -47,8 +48,9 @@ class CancelOrderAfterPassedTime extends Command
         DB::beginTransaction();
 
         try {
-            $sitter_orders = SitterOrder::whereIn('status', [Statuses::WAITING, Statuses::PROCESS])->get();
-            $center_orders = CenterOrder::whereIn('status', [Statuses::WAITING, Statuses::PROCESS])->get();
+            $sitter_orders = SitterOrder::get();
+            $center_orders = CenterOrder::get();
+
             if ($sitter_orders->count() > 0) {
                 foreach ($sitter_orders as $sitter_order) {
                     if (optional($sitter_order->service)->service_type == 'hour') {
@@ -59,16 +61,20 @@ class CancelOrderAfterPassedTime extends Command
                             $this->chargeWallet(optional($sitter_order->main_order)->price_after_offer, $sitter_order->client_id);
                         }
                     } else {
-
-                        Log::info($sitter_order->months->month_dates);
+                        // Log::info(OrderMonthDate::where('order_month_id',optional($sitter_order->months)->id)->where('status','<>', 'completed')->where('date', '<', now()->format('Y-m-d'))->get());
+                        // Log::info(optional($sitter_order->months)->id);
                         if ($sitter_order->months && $sitter_order->months->month_dates->count() > 0) {
                             // Log::info($sitter_order->months);
-
-                            $sitter_order_month = $sitter_order->months->month_dates()->where('order_month_dates.status','<>', 'completed')->where('order_month_dates.date', '<', now()->format('Y-m-d'))->firstOrFail();
+                            $sitter_order_month = OrderMonthDate::where('order_month_id',optional($sitter_order->months)->id)->where('status','<>', 'completed')->where('date', '<', now()->format('Y-m-d'))->get();
+                            // $sitter_order_month = $sitter_order->months->month_dates()->where('order_month_dates.status', '<>', 'completed')->where('order_month_dates.date', '<', now()->format('Y-m-d'))->first();
                             // $sitter_order_month->update(['status'=>'canceled']);
-                            Log::info($sitter_order_month);
+                            // Log::info($sitter_order_month);
+                            if (isset($sitter_order_month) && $sitter_order_month) {
+                               foreach($sitter_order_month as $date){
 
-                            $sitter_order_month->update(['status' => 'canceled']);
+                                   $date->update(['status' => 'canceled']);
+                               }
+                            }
                             // Log::info($hours*optional($sitter_order->months)->price_per_hour_for_month);
                         }
                     }
@@ -85,13 +91,16 @@ class CancelOrderAfterPassedTime extends Command
                         }
                     } else {
 
-
+                        // Log::info($center_order->months->id);
                         if ($center_order->months) {
 
-                            $center_order_month = $center_order->months->month_dates()->where('order_month_dates.status','<>', 'completed')->where('order_month_dates.date', '<', now()->format('Y-m-d'))->firstOrFail();
+                            $center_order_month = OrderMonthDate::where('order_month_id',optional($center_order->months)->id)->where('status','<>', 'completed')->where('date', '<', now()->format('Y-m-d'))->get();
+                            if (isset($sitter_order_month) && $sitter_order_month) {
+                                foreach($center_order_month as $date){
 
-                            $center_order_month->update(['status' => 'canceled']);
-
+                                    $date->update(['status' => 'canceled']);
+                                }
+                            }
                             // Log::info($hours*optional($sitter_order->months)->price_per_hour_for_month);
                         }
                     }
